317e2880cb66ffdd734615b31774fac4c45251d2
Hibikine Kage <goodsmile314159@gmail.com>
2017-12-28 02:55:56 +0900

    ログアウトつけた

--- src/login.js
+++ src/login.js
@@ -9,6 +9,13 @@ loginButton.attr('href', './server.php');
 if (isLogin()) {
   loginButton.hide();
   $('.old-tweet-login').hide();
+  const logoutButton = $('<button id="logout-button" class="btn btn-link">ログアウト</button>');
+  logoutButton.click(() => {
+    Cookies.remove('is_login', { path: '' });
+    Cookies.remove('PHPSESSID', { path: '' });
+    location.reload();
+  });
+  $('#navbars').append($('<p class="d-inline my-2 my-lg-0"></p>').append(logoutButton));
 }
 loginButton.click(() => {
   gtag('event', 'login');
