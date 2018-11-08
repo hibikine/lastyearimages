da37fcddfba242ae5fb5c2850c1d2ebecb148e12
Hibikine Kage <HibikineKage@users.noreply.github.com>
2018-11-09 00:03:28 +0900

    feat: add app tsx (#18)

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
 import * as React from 'react';
-import {render}from'react-dom'
-render(<div>準備中だよ！</div>, document.getElementById('root'))
+import { render } from 'react-dom';
+import App from './app';
+
+render(<App />, document.getElementById('root'));
