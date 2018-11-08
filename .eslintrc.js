5c490b49971e2d11b7819292c85eb71318bc55c3
Hibikine Kage <HibikineKage@users.noreply.github.com>
2018-11-09 01:50:23 +0900

    chore: add lint (#21)
    * chore: add lint
    
    * add typescript eslint settings
    
    * commit 忘れ
    
    * add import nounresolved setting to eslint
    
    * import no unresolved off
    
    * eslintrc add import extensions to off
    
    * fix: eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,32 @@
 module.exports = {
-    "extends": "airbnb",
-    "rules": {
-        "no-mixed-operators": "off",
-        "no-multi-assign": "off",
-        "no-underscore-dangle": "off",
-        "no-bitwise": "off",
-        "int32Hint": true,
+  extends: ['airbnb'],
+  parser: 'typescript-eslint-parser',
+  parserOptions: {
+    jsx: true,
+    useJSXTextNode: true,
+  },
+  rules: {
+    'no-mixed-operators': 'off',
+    'no-multi-assign': 'off',
+    'no-underscore-dangle': 'off',
+    'no-bitwise': 'off',
+    int32Hint: true,
+    'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+    'import/extensions': ['.js', 'jsx', '.json', '.ts', '.tsx'],
+  },
+  globals: {
+    window: true,
+    document: true,
+    Image: true,
+    gtag: true,
+    Cookies: true,
+    fetch: true,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
     },
-    "globals": {
-        "window": true,
-        "document": true,
-        "Image": true,
-        "gtag": true,
-        "Cookies": true,
-        "fetch": true,
-    }
+  },
 };
