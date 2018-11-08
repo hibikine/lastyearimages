10d3a31af551dc4bd8f307514f6f826dac1ec130
Hibikine Kage <goodsmile314159@gmail.com>
2018-11-09 01:40:31 +0900

    tmp

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,18 @@
 import * as React from 'react';
 import { TwitterPicker } from 'react-color';
+import rmwoff2 from '../fonts/rounded-x-mplus-1p-bold.woff2';
+import rmwoff from '../fonts/rounded-x-mplus-1p-bold.woff';
+import { injectGlobal } from 'emotion';
+
+injectGlobal`
+@font-face {
+  font-family: 'Rounded M+ 1p Bold';
+  font-style: bold;
+  font-weight: 700;
+  src: local('Rounded M+ 1p Bold'),
+  url()
+}
+`;
 
 const App = () => (
   <div>
